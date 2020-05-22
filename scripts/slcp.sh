d815ce61a6690f57540d13af743824e4dfb0ca09
nishimoto <a32b16c4@gmail.com>
2020-05-22 19:19:05 +0900

    script

--- scripts/slcp.sh
+++ scripts/slcp.sh
@@ -11,6 +11,11 @@ filearr=( "fib41" "rs.13" "tm29" "dblp.xml.00001.1" "dblp.xml.00001.2" "dblp.xml
 
 for file in ${filearr[@]}; do
 	echo "sclp test new (sdsl) ${file}"
-    nohup /usr/bin/time -f "#slcp test new (sdsl) ${file}, %e sec, %M KB" ~/rlbwt_iterator/build/time_lcp.out -i ${outputpath}/${file}.rlbwt -m sdsl >> /mnt/server/nishimoto/result/slcp_new_output.log
+    nohup /usr/bin/time -f "#slcp test new (sdsl) ${file}, %e sec, %M KB" ~/rlbwt_iterator/build/time_lcp.out -i ${outputpath}/${file}.rlbwt -m new >> /mnt/server/nishimoto/result/slcp0_new_output.log
+done
+
+for file in ${filearr[@]}; do
+	echo "sclp test new (sdsl) ${file}"
+    nohup /usr/bin/time -f "#slcp test new (sdsl) ${file}, %e sec, %M KB" ~/rlbwt_iterator/build/time_lcp.out -i ${outputpath}/${file}.rlbwt -m weiner >> /mnt/server/nishimoto/result/slcp0_weiner_output.log
 done
 
