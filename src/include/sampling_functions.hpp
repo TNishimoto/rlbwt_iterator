53b910f02a4cc82ef8cc19234725febed0c6a100
nishimoto <a32b16c4@gmail.com>
2020-05-24 18:38:04 +0900

    ssa

--- src/include/sampling_functions.hpp
+++ src/include/sampling_functions.hpp
@@ -20,7 +20,7 @@ namespace stool
             uint64_t hole_pos;
             uint64_t hole_length;
         };
-
+        /*
         struct LPoint
         {
             uint64_t index;
@@ -31,12 +31,14 @@ namespace stool
                 this->diff = _diff;
             }
         };
+        */
         struct SamplingSAStackData
         {
-            LPoint start;
-            LPoint end;
+            uint64_t start;
             uint64_t distance;
+            bool is_start;
         };
+        /*
         struct SamplingSAStackData1
         {
             uint64_t source;
@@ -45,6 +47,7 @@ namespace stool
             bool is_source_first;
             bool is_destination_first;
         };
+        */
         /*
         template <typename RLBWT_STR, typename INDEX = typename RLBWT_STR::index_type>
         class LFDataStructure{
@@ -209,9 +212,36 @@ namespace stool
 
         public:
             template <typename RLBWT_STR, typename INDEX = typename RLBWT_STR::index_type, typename INDEX2 = typename RLBWT_STR::index_type>
+            static SamplingSAStackData local_function(const RLBWT_STR &rlbwt, std::vector<INDEX> &hole_pos_array, std::vector<INDEX> &hole_length_array, SamplingSAStackData &input)
+            {
+                SamplingSAStackData output;
+
+                uint64_t lindex = input.start;
+                uint64_t diff = input.is_start ? 0 : (rlbwt.get_run(lindex) - 1);
+                uint64_t pos = 0;
+                uint64_t distance = 0;
+                //bool b = rlbwt.get_run(lindex) == 1;
+                while (true)
+                {
+                    pos = hole_pos_array[lindex] + diff;
+                    distance += hole_length_array[lindex];
+                    lindex = rlbwt.get_lindex_containing_the_position(pos);
+                    diff = pos - rlbwt.get_lpos(lindex);
+                    if (diff == 0 || diff == rlbwt.get_run(lindex) - 1)
+                    {
+                        output.start = lindex;
+                        output.is_start = diff == 0;
+                        output.distance = distance;
+                        break;
+                    }
+                }
+                return output;
+            }
+
+            template <typename RLBWT_STR, typename INDEX = typename RLBWT_STR::index_type, typename INDEX2 = typename RLBWT_STR::index_type>
             static std::pair<std::vector<INDEX2>, std::vector<INDEX2>> construct_sampling_sa_lorder(const RLBWT_STR &rlbwt, std::vector<INDEX> &hole_pos_array, std::vector<INDEX> &hole_length_array)
             {
-                using STACK_POS = std::pair<INDEX2, bool>;
+                //using STACK_POS = std::pair<INDEX2, bool>;
                 using CHAR = typename RLBWT_STR::char_type;
                 INDEX2 MAXFLAG = std::numeric_limits<INDEX2>::max();
                 std::pair<std::vector<INDEX2>, std::vector<INDEX2>> r;
@@ -224,89 +254,107 @@ namespace stool
 
                 uint64_t total = 0;
 
-                std::stack<STACK_POS> stack;
-
-                for (INDEX i = 0; i < rleSize; i++)
+                std::stack<SamplingSAStackData> stack;
+                uint64_t i = 0;
+                bool is_i_start = true;
+                    SamplingSAStackData ssd;
+                while (i < rleSize)
                 {
-                    if (beginVec[i] == MAXFLAG)
+                    bool i_is_empty = false;
+                    if (is_i_start && beginVec[i] == MAXFLAG)
                     {
-                        stack.push(STACK_POS(i, true));
+                        ssd.start = i;
+                        ssd.is_start = true;
+                        ssd.distance = 0;
+                        i_is_empty = true;
                     }
-                    if (endVec[i] == MAXFLAG && rlbwt.get_run(i) != 1)
+                    else if (!is_i_start && endVec[i] == MAXFLAG && rlbwt.get_run(i) != 1)
                     {
-                        stack.push(STACK_POS(i, false));
+                        SamplingSAStackData ssd;
+                        ssd.start = i;
+                        ssd.is_start = false;
+                        ssd.distance = 0;
+                        stack.push(ssd);
+                        i_is_empty = true;
                     }
 
-                    while (stack.size() > 0)
+                    if (i_is_empty)
                     {
-                        STACK_POS top = stack.top();
-
-                        uint64_t lindex = top.first;
-                        uint64_t diff = top.second ? 0 : (rlbwt.get_run(lindex) - 1);
-                        uint64_t pos = 0;
-                        uint64_t distance = 0;
-                        bool b = rlbwt.get_run(lindex) == 1;
-
-                        if(stack.size() % 100000 == 0){
-                            std::cout << "[" << total << ", " << (rleSize * 2) << ", " << stack.size() << "]" << std::endl;
-                        }
-
+                        uint64_t prev_sa_value = UINT64_MAX;
                         while (true)
                         {
-                            pos = hole_pos_array[lindex] + diff;
-                            distance += hole_length_array[lindex];
-                            lindex = rlbwt.get_lindex_containing_the_position(pos);
-                            diff = pos - rlbwt.get_lpos(lindex);
-                            CHAR c = rlbwt.get_char_by_run_index(lindex);
-
-                            if (diff == 0 || diff == rlbwt.get_run(lindex) - 1)
+                            auto output = local_function(rlbwt, hole_pos_array, hole_length_array, ssd);
+                            ssd.distance = output.distance;
+                            CHAR c = rlbwt.get_char_by_run_index(output.start);
+                            stack.push(ssd);
+                            if (c == 0)
                             {
-                                if (c == 0)
-                                {
-                                    if (top.second)
-                                    {
-                                        beginVec[top.first] = distance - 1;
-                                    }
-                                    if ((top.second && b) || !top.second)
-                                    {
-                                        endVec[top.first] = distance - 1;
-                                    }
-                                    stack.pop();
-                                    total++;
-                                }
-                                else if (diff == 0 && beginVec[lindex] != MAXFLAG)
-                                {
-                                    if (top.second)
-                                    {
-                                        beginVec[top.first] = beginVec[lindex] + distance;
-                                    }
-                                    if ((top.second && b) || !top.second)
-                                    {
-                                        endVec[top.first] = beginVec[lindex] + distance;
-                                    }
-                                    stack.pop();
-                                    total++;
-                                }
-                                else if (diff != 0 && endVec[lindex] != MAXFLAG)
-                                {
-                                    if (top.second)
-                                    {
-                                        beginVec[top.first] = endVec[lindex] + distance;
-                                    }
-                                    if ((top.second && b) || !top.second)
-                                    {
-                                        endVec[top.first] = endVec[lindex] + distance;
-                                    }
-                                    stack.pop();
-                                    total++;
-                                }
-                                else
-                                {
-                                    stack.push(STACK_POS(lindex, diff == 0));
-                                }
+                                prev_sa_value = rlbwt.str_size() - 1;
                                 break;
                             }
+                            else if (output.is_start && beginVec[output.start] != MAXFLAG)
+                            {
+                                prev_sa_value = beginVec[output.start];
+                                break;
+                            }
+                            else if (!output.is_start && endVec[output.start] != MAXFLAG)
+                            {
+                                prev_sa_value = endVec[output.start];
+                                break;
+                            }
+                            else
+                            {
+                                ssd.start = output.start;
+                                ssd.is_start = output.is_start;
+                                ssd.distance = 0;
+                            }
+                            /*
+                            if (stack.size() % 100000 == 0)
+                            {
+                                std::cout << i << "/" << stack.size() << ", " << (rleSize * 2) << std::endl;
+                            }
+                            */
                         }
+                        while (stack.size() > 0)
+                        {
+                            /*
+                            if (stack.size() % 100000 == 0)
+                            {
+                                std::cout << stack.size() << ", " << (rleSize * 2) << std::endl;
+                            }
+                            */
+
+                            auto top = stack.top();
+                            stack.pop();
+                            bool b = rlbwt.get_run(top.start) == 1;
+
+                            if (prev_sa_value != rlbwt.str_size() - 1)
+                            {
+                                prev_sa_value = prev_sa_value + top.distance;
+                            }
+                            else
+                            {
+                                prev_sa_value = top.distance - 1;
+                            }
+                            if (top.is_start)
+                            {
+                                beginVec[top.start] = prev_sa_value;
+                            }
+                            if ((top.is_start && b) || !top.is_start)
+                            {
+                                endVec[top.start] = prev_sa_value;
+                            }
+                        }
+                    }
+
+                    if (is_i_start)
+                    {
+                        is_i_start = false;
+                    }
+                    else
+                    {
+                        is_i_start = true;
+                        i++;
                     }
                 }
                 return r;
